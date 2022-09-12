f1267fc7047f057fd4eaf0d0a2bd39869c2c8a87
younchong <ungun96@naver.com>
2022-09-12 19:19:34 +0900

    chore🧱 : deploy위한 요청 주소 변경

--- utils/getSignalData.ts
+++ utils/getSignalData.ts
@@ -4,7 +4,7 @@ interface position {
 }
 
 export default async function getSignalData(position: position) {
-  const data = await fetch("http://localhost:3000/api/signal", {
+  const data = await fetch(window.location.origin + "/api/signal", {
     method: "POST",
     body: JSON.stringify(position),
   });
